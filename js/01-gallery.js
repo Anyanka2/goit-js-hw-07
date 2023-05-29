0d59c3403ff756a35991f426c07ed93a57cf0805
Anyanka2 <123205369+Anyanka2@users.noreply.github.com>
2023-05-29 20:53:55 +0200

    edit rewiews

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,26 +1,21 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
 const galleryContainer = document.querySelector('.gallery');
 galleryContainer.addEventListener('click', onGalleryClick);
 let modalBox;
-document.addEventListener('keydown', onPressEsc);
-
-galleryItems.forEach(el => {
-  let galleryItem = document.createElement('li');
-  galleryItem.classList.add('gallery__item');
-  let galleryLink = document.createElement('a');
-  galleryLink.classList.add('gallery__link');
-  galleryLink.setAttribute('href', el.original);
-  galleryItem.append(galleryLink);
-  let galleryImage = document.createElement('img');
-  galleryImage.classList.add('gallery__image');
-  galleryImage.setAttribute('src', el.preview);
-  galleryImage.setAttribute('alt', el.description);
-  galleryImage.setAttribute('data-source', el.original);
-  galleryLink.append(galleryImage);
-  galleryContainer.append(galleryItem);
-});
+const makeGalleryItems = ({ preview, original, description }) =>
+  `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`;
+const galleryList = galleryItems.map(el => makeGalleryItems(el)).join('');
+galleryContainer.insertAdjacentHTML('afterbegin', galleryList);
 
 function onGalleryClick(event) {
   event.preventDefault();
@@ -32,6 +27,14 @@ function onGalleryClick(event) {
     `
 		<img width="1400" height="900" src="${originalImg}">
 	`,
+    {
+      onShow: () => {
+        document.addEventListener('keydown', onPressEsc);
+      },
+      onClose: () => {
+        document.removeEventListener('keydown', onPressEsc);
+      },
+    },
   );
   modalBox.show();
 }
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,22 +1,22 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
 const galleryContainer = document.querySelector('.gallery');
-galleryItems.forEach(el => {
-  let galleryItem = document.createElement('li');
-  galleryItem.classList.add('gallery__item');
-  let galleryLink = document.createElement('a');
-  galleryLink.classList.add('gallery__link');
-  galleryLink.setAttribute('href', el.original);
-  galleryItem.append(galleryLink);
-  let galleryImage = document.createElement('img');
-  galleryImage.classList.add('gallery__image');
-  galleryImage.setAttribute('src', el.preview);
-  galleryImage.setAttribute('alt', el.description);
-  galleryLink.append(galleryImage);
-  galleryContainer.append(galleryItem);
-});
-var lightbox = new SimpleLightbox('.gallery a', {
-    captionsData: 'alt',
-    captionDelay: 250
+
+const makeGalleryItems = ({ preview, original, description }) =>
+  `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`;
+const galleryList = galleryItems.map(el => makeGalleryItems(el)).join('');
+galleryContainer.insertAdjacentHTML('afterbegin', galleryList);
+
+let lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
 });
